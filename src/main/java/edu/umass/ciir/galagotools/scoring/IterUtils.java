47611ff9b7bbc951c724b003ceb38f91ccb04715
John Foley <jfoley@cs.umass.edu>
2014-08-06 13:08:57 -0400

    Add traversal to Parameters

--- src/main/java/edu/umass/ciir/galagotools/scoring/IterUtils.java
+++ src/main/java/edu/umass/ciir/galagotools/scoring/IterUtils.java
@@ -1,6 +1,8 @@
 package edu.umass.ciir.galagotools.scoring;
 
+import edu.umass.ciir.galagotools.utils.Util;
 import org.lemurproject.galago.core.retrieval.iterator.BaseIterator;
+import org.lemurproject.galago.core.retrieval.traversal.Traversal;
 import org.lemurproject.galago.utility.Parameters;
 
 /**
@@ -12,5 +14,11 @@ public class IterUtils {
       p.put("operators", Parameters.instance());
     }
     p.getMap("operators").put(name, iterClass.getName());
+  }
+
+  public static void addToParameters(Parameters argp, Class<? extends Traversal> traversalClass) {
+    Util.extendList(argp, "traversals", Parameters.class, Parameters.parseArray(
+      "name", traversalClass.getName()
+    ));
   }
 }
